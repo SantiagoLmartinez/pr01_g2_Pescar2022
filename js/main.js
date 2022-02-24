4f26c289d0c4b4c7bc654a44d22b169d5ddfcd3e
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-24 19:42:41 -0300

    probando sacar datos de la db y ponerlos en DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('javascript conectado al html')
+// console.log('javascript conectado al html')
+const _mascotaContainer = document.querySelector('#mascotaContainer')
+
+_mascotaContainer.innerHTML = `
+<div class="card" style="width: 18rem;">
+<img src="${mascotasDb[0].img}" class="card-img-top" alt="img-mascota">
+<div class="card-body">
+    <h5 class="card-title">${mascotasDb[0].tipo}  </h5>
+    <p class="card-text">${mascotasDb[0].observaciones}</p>
+    <a href="#" class="btn btn-primary w-100">Adoptar</a>
+</div>
+</div>
+`
