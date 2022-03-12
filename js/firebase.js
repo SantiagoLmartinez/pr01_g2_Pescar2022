e00f8e31f63f6e644afd70c18c4ee7587c30a98b
JosePerezHuanca <perezhuancajose@gmail.com>
2022-03-12 10:47:44 -0300

    Se agregan las credenciales para conectar a firebase

---
+++ js/firebase.js
@@ -0,0 +1,26 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
+  import {  getFirestore,
+    collection,
+    getDocs,
+    onSnapshot,
+    addDoc,
+    deleteDoc,
+    doc,
+    getDoc,
+    updateDoc} from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCvd_OuvhLe-ATtOxDO_0ey94LjEeHZHkU",
+    authDomain: "huellitas-a16de.firebaseapp.com",
+    projectId: "huellitas-a16de",
+    storageBucket: "huellitas-a16de.appspot.com",
+    messagingSenderId: "85264667945",
+    appId: "1:85264667945:web:6ba46f4e0572b34d45d338"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
