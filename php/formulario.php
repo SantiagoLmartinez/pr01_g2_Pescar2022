484de021bcafb3856cdbaefd06bee7e4a0332575
Dylan Quiroga <dyalanquiroga98@gmail.com>
2022-03-24 20:52:19 -0300

    adicion de carga de imagenes a la db

--- php/formulario.php
+++ php/formulario.php
@@ -10,21 +10,22 @@ header('Location: https://huellitas-grupo2.000webhostapp.com/huellitas/formulari
 require_once('conexion.php');
 
 
-$nombre=$_GET['nombre'];
-$tipo_animal=$_GET['tipo_animal'];
-$edad=$_GET['edad'];
+$nombre=$_POST['nombre'];
+$tipo_animal=$_POST['tipo_animal'];
+$edad=$_POST['edad'];
+$imagen= addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
 
 
 $conn=new conexion();
 
-//INSERT INTO `mascotas` (`nombre`, `tipo`, `edad`) VALUES ('collin', 'perro', '8');
+//INSERT INTO `mascotas`(`nombre`, `tipo`, `edad`, `imagen`) VALUES ([value-2],[value-3],[value-4],[value-5])
 //UPDATE `mascotas` SET `nombre`='collin',`tipo`='perro',`edad`='8' WHERE 1;
 
 //Hacemos la consulta de SQL para actualizar tabla mascotas
 //$queryUPDATE="UPDATE `mascotas` SET `tipo` = '$tipo_animal', `edad` = '$edad' WHERE `mascotas`.`nombre` = '$nombre';";
 
 //Hacemos la consulta de SQL para insertar una nueva mascota
-$queryUPDATE="INSERT INTO `mascotas` (`nombre`,`tipo`,`edad`) VALUES ('$nombre', '$tipo_animal', '$edad');";
+$queryUPDATE="INSERT INTO `mascotas` (`nombre`,`tipo`,`edad`, `imagen`) VALUES ('$nombre', '$tipo_animal', '$edad', '$imagen');";
 
 //primer parametro la conexion, el segundo la consulta
 $update= mysqli_query($conn->conectardb(),$queryUPDATE);
