80a0e919300a387e8c8ec8bceb6b26e677b3f441
JosePerezHuanca <perezhuancajose@gmail.com>
2022-04-16 11:08:37 -0300

    Se borraron algunos archivos que ya no se usan

--- routes/db.js
+++ routes/db.js
@@ -15,13 +15,17 @@ conexion.connect(function(error){
     }
 })
 
-conexion.query('create table mascotas(nombre varchar(20), tipo varchar(10), edad integer, cantidad varchar(10), sexo varchar(6), esCastrado varchar(6), condicion varchar(5), condicionDescripcion varchar(500), imagen varchar(100), duenio varchar(20), mail varchar(40), localidad varchar(80), comentarios varchar(500))', function(error, resultado){
-if(error){
-    console.log('Error en la consulta o la tabla ya existe');
+function crearTabla(){
+    conexion.query('create table mascotas(nombre varchar(20), tipo varchar(10), edad integer, cantidad varchar(10), sexo varchar(6), esCastrado varchar(6), condicion varchar(5), condicionDescripcion varchar(500), imagen varchar(100), duenio varchar(20), mail varchar(40), localidad varchar(80), comentarios varchar(500))', function(error, resultado){
+        if(error){
+            console.log('Error en la consulta o la tabla ya existe');
+        }
+        else{
+            console.log('Se creo la tabla mascotas');
+        }
+    })
 }
-else{
-    console.log('Se creo la tabla mascotas');
-}
-})
+
+crearTabla();
 
 module.exports=conexion
